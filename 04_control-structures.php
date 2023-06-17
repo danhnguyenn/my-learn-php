aa3032c75922800bad9961beea3f92130d3c811b
Danh Nguyen <danh.nguyentrancong>
2023-06-17 23:20:22 +0700

    if-else

---
+++ 04_control-structures.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control Structures</title>
+</head>
+<body>
+    <?php 
+       $roll1 = rand(1, 6);
+       $roll2 = rand(1, 6);
+       echo '<p>You rolled a ' . $roll1 . ' and a ' . $roll2 . '</p>';
+       if ($roll1 == 6 || $roll2 == 6) {
+        echo '<p>You win!</p>';
+       }
+       else {
+        echo '<p>Sorry, you didn\'t win. Better luck next time!</p>';
+       }
+        echo '<p>Thanks for playing</p>';
+    ?>
+</body>
+</html>
